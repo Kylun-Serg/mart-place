edc5f75ed6cf5adf8902c3aa32c3f7ba155ca56d
Kylun-Serg <kylun1serg@gmail.com>
2019-12-01 19:31:15 +0200

    add slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,22 @@
 $(function(){
-	var mixer = mixitup('.products__inner-box');
+	
+	$('select').styler();
+
+	$(".features-slider__item-footer-star").rateYo({
+    	rating: 4.5,
+    	
+    	starWidth: "17px",
+    	normalFill: "#f6e4ac",
+    	ratedFill: "#ffc000"
+  });
+
+	$('.features-slider__inner').slick({
+		dots: false,
+  		infinite: true,
+  		speed: 800,
+  		slidesToShow: 1,
+  		nextArrow: '<button class="slick-arrow slick-next"><img src="../images/features-slider-item/arrow-right.svg" alt=""></button>',
+  		prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/features-slider-item/arrow-left.svg" alt=""></button>',
+	});
+
 });
